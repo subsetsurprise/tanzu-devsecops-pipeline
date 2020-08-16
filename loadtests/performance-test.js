392831d5ca0c6cd883e6c4d7d4884ba1dee02cbc
subsetsurprise <tstacey@pivotal.io>
2020-08-16 14:20:41 -0400

    Added custom metrics for failure rate

--- loadtests/performance-test.js
+++ loadtests/performance-test.js
@@ -2,16 +2,21 @@
 
 import { sleep } from "k6";
 import http from "k6/http";
+import { Rate } from 'k6/metrics';
+
+const myFailRate = new Rate('failed requests');
 
 export let options = {
   duration: "5m",
   vus: 250,
   thresholds: {
-    http_req_duration: ["p(95)<500"]
+    'failed requests': ['rate<0.1'],
+    'http_req_duration': ["p(95)<500"]
   }
 };
 
 export default function () {
-  http.get("https://tanzu-build.apps.tanzu.on-target.tech");
-  sleep(1);
+  let res = http.get("https://tanzu-build.apps.tanzu.on-target.tech");
+  myFailRate.add(res.status !== 200);
+  sleep(0.25);
 }
